300ca82b2081cceb22696a4afa4f1485743356c5
Dan Gregory <danielgregory1986@googlemail.com>
2018-08-04 17:30:32 +0100

    Implemented UI tests using the Espresso test recorder.

---
+++ app/src/androidTest/java/gregory/dan/mybakingapp/MainActivityTabletTest.java
@@ -0,0 +1,174 @@
+package gregory.dan.mybakingapp;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTabletTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTabletTest() {
+        ViewInteraction recyclerView = onView(
+                allOf(withId(R.id.activity_main_recycler_view),
+                        childAtPosition(
+                                withClassName(is("android.support.constraint.ConstraintLayout")),
+                                0)));
+        recyclerView.perform(actionOnItemAtPosition(1, click()));
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(700);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.content), withText("Recipe Introduction"),
+                        childAtPosition(
+                                allOf(withId(R.id.step_list_item),
+                                        childAtPosition(
+                                                withId(R.id.recipeinstruction_list),
+                                                1)),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(withText("Recipe Introduction")));
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.content), withText("Starting prep"),
+                        childAtPosition(
+                                allOf(withId(R.id.step_list_item),
+                                        childAtPosition(
+                                                withId(R.id.recipeinstruction_list),
+                                                2)),
+                                0),
+                        isDisplayed()));
+        textView2.check(matches(withText("Starting prep")));
+
+        ViewInteraction textView3 = onView(
+                allOf(withId(R.id.content), withText("Starting prep"),
+                        childAtPosition(
+                                allOf(withId(R.id.step_list_item),
+                                        childAtPosition(
+                                                withId(R.id.recipeinstruction_list),
+                                                2)),
+                                0),
+                        isDisplayed()));
+        textView3.check(matches(withText("Starting prep")));
+
+        ViewInteraction recyclerView2 = onView(
+                allOf(withId(R.id.recipeinstruction_list),
+                        childAtPosition(
+                                withClassName(is("android.widget.LinearLayout")),
+                                0)));
+        recyclerView2.perform(actionOnItemAtPosition(0, click()));
+
+        ViewInteraction recyclerView3 = onView(
+                allOf(withId(R.id.recipeinstruction_list),
+                        childAtPosition(
+                                withClassName(is("android.widget.LinearLayout")),
+                                0)));
+        recyclerView3.perform(actionOnItemAtPosition(2, click()));
+
+        ViewInteraction textView4 = onView(
+                allOf(withId(R.id.detail_step_text_view), withText("1. Preheat the oven to 350�F. Butter the bottom and sides of a 9\"x13\" pan."),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.recipeinstruction_detail_container),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textView4.check(matches(isDisplayed()));
+
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Navigate up"),
+                        childAtPosition(
+                                allOf(withId(R.id.action_bar),
+                                        childAtPosition(
+                                                withId(R.id.action_bar_container),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        // Added a sleep statement to match the app's execution delay.
+        // The recommended way to handle such scenarios is to use Espresso idling resources:
+        // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
+        try {
+            Thread.sleep(700);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        ViewInteraction frameLayout = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.activity_main_recycler_view),
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.view.ViewGroup.class),
+                                        0)),
+                        0),
+                        isDisplayed()));
+        frameLayout.check(matches(isDisplayed()));
+
+        ViewInteraction recyclerView4 = onView(
+                allOf(withId(R.id.activity_main_recycler_view),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                0),
+                        isDisplayed()));
+        recyclerView4.check(matches(isDisplayed()));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
